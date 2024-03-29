4aed4be2266412141dbf6472b17d105a83e2bbd1
Joris van de Donk <jorisvddonk@gmail.com>
2024-03-29 15:55:23 +0300

    Update to Discord.js v14

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 1. `yarn`
 2. `npm install -g ts-node typescript`
-3. Create `config.json` containing: `{"token": "...discord bot token here ..."}`
+3. Create `config.json` containing: `{"token": "...discord bot token here ...", "clientId": "...discord bot application id here ..."}`
 4. `ts-node index.ts`
--- index.ts
+++ index.ts
@@ -1,37 +1,43 @@
 import * as fs from "fs";
 const config = JSON.parse(fs.readFileSync("./config.json").toString());
-import * as Discord from "discord.js";
+import { Client, Events, EmbedBuilder, GatewayIntentBits, SlashCommandBuilder, REST, Routes, Collection } from "discord.js";
 import { Noctis } from "./noctis_data";
 
-const client = new Discord.Client();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const Universe = new Noctis();
+const rest = new REST().setToken(config.token);
 
 const makeEmbed = (title: string, data: string) => {
-  return new Discord.RichEmbed()
+  return new EmbedBuilder()
     .setColor("#0099ff")
-    .setAuthor("GOESDNET", undefined, "https://github.com/jorisvddonk/goesdnet")
+    .setTitle(title)
+    .setAuthor({ name: "GOESDNET", url: "https://github.com/jorisvddonk/goesdnet", iconURL: "http://mooses.nl/nice/misc/noctisiv_logo.png" })
     .setThumbnail("http://mooses.nl/nice/misc/noctisiv_logo.png")
-    .addField(title, data);
+    .addFields([{ name: 'data', value: data, inline: true }]);
 };
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const lookupCommand = {
+  data: new SlashCommandBuilder()
+    .setName('lookup')
+    .setDescription('Look up coordinates of star/planet')
+    .addStringOption(option =>
+      option.setName('name')
+        .setRequired(true)
+        .setDescription('The name to look up in the Starmap')),
+  async execute(interaction) {
+    const tgt = interaction.options.getString('name');
 
-client.on("message", msg => {
-  if (msg.content.indexOf("!lookup ") === 0) {
-    const tgt = msg.content.replace("!lookup ", "");
     const star = Universe.getStarByName(tgt);
     const planet = Universe.getPlanetByName(tgt);
 
     if (star !== undefined) {
       const embed = makeEmbed(
-        `Star S${star.type} ${star.name} found at:`,
-        `> x: ${star.x}\n> y: ${star.y}\n> z: ${star.z}`
+        `Star S${star.type} ${star.name} found at`,
+        `x: ${star.x}\ny: ${star.y}\nz: ${star.z}`
       );
       const guideEntries = Universe.getGuideEntriesForStar(tgt);
-      embed.setFooter(`${guideEntries.length} guide entries available`);
-      msg.channel.send(embed);
+      embed.setFooter({ text: `${guideEntries.length} guide entries available` });
+      await interaction.reply({ embeds: [embed] });
     } else if (planet !== undefined) {
       const starid = Universe.getIDForStarCoordinates(
         planet.x,
@@ -40,68 +46,116 @@ client.on("message", msg => {
       );
       const parentstar = Universe.getStarByID(starid);
       const embed = makeEmbed(
-        `Planet P${planet.index} ${planet.name} ${
-          parentstar !== undefined
-            ? "found orbiting " + parentstar.name
-            : "orbiting around unknown star"
-        } at:`,
-        `> x: ${planet.x}\n> y: ${planet.y}\n> z: ${planet.z}`
+        `Planet P${planet.index} ${planet.name} ${parentstar !== undefined
+          ? "found orbiting " + parentstar.name
+          : "orbiting around unknown star"
+        } at`,
+        `x: ${planet.x}\ny: ${planet.y}\nz: ${planet.z}`
       );
       const guideEntries = Universe.getGuideEntriesForPlanetByName(planet.name);
-      embed.setFooter(`${guideEntries.length} guide entries available`);
-      msg.channel.send(embed);
+      embed.setFooter({ text: `${guideEntries.length} guide entries available` });
+      await interaction.reply({ embeds: [embed] });
     } else {
-      msg.channel.send("No object found!");
+      await interaction.reply("No object found!");
+    }
+  },
+};
+
+const guideCommand = {
+  data: new SlashCommandBuilder()
+    .setName('guide')
+    .setDescription('Look up text in the Guide')
+    .addStringOption(option =>
+      option.setName('object_name')
+        .setRequired(true)
+        .setDescription('The name to look up in the Guide'))
+    .addNumberOption(option =>
+      option.setName('start_offset')
+        .setRequired(false)
+        .setDescription('The offset of the first entry to show')),
+  async execute(interaction) {
+    const tgt = interaction.options.getString('object_name');
+    const offset = interaction.options.getNumber('start_offset');
+
+
+    let guideEntries = Universe.getGuideEntriesForStar(
+      Universe.getIDForStar(tgt)
+    );
+    if (guideEntries.length === 0) {
+      guideEntries = Universe.getGuideEntriesForPlanetByName(tgt);
+    }
+
+    let entriesText = "No entries found in GUIDE";
+    let entriesFooter = null;
+    if (guideEntries.length > 0) {
+      const offsetMax = offset + 10;
+      const displayGuideEntries = guideEntries.filter((x, index) => {
+        return index >= offset && index < offsetMax;
+      });
+      entriesText = displayGuideEntries.map(x => `> ${x.text}`).join("\n");
+      const remainingEntries = guideEntries.length - offsetMax;
+      if (remainingEntries > 0) {
+        entriesFooter = `${remainingEntries} more entries available.`;
+      } else {
+        entriesFooter = "No more entries remaining";
+      }
+    }
+    if (entriesText.length > 0) {
+      const embed = makeEmbed(
+        `Guide entries for ${tgt.toUpperCase()}`,
+        entriesText
+      );
+      if (entriesFooter) {
+        embed.setFooter({ text: entriesFooter });
+      }
+      await interaction.reply({ embeds: [embed] });
+    } else {
+      await interaction.reply(`No entries available for ${tgt.toUpperCase()}`);
     }
   }
+};
 
-  if (msg.content.indexOf("!guide ") === 0) {
-    const rmsg = msg.content.replace("!guide ", "");
-    const tgtRE = rmsg.match(/([a-zA-Z0-9 ]+)/);
-    if (tgtRE) {
-      const tgt = tgtRE[0].trim();
-      const offsetRE = rmsg.match(/\(\+(\d+)\)/);
-      let offset = 0;
-      if (offsetRE) {
-        offset = parseInt(offsetRE[1]);
-      }
+const commands = new Collection();
+commands.set(lookupCommand.data.name, lookupCommand);
+commands.set(guideCommand.data.name, guideCommand);
+(client as any).commands = commands;
 
-      let guideEntries = Universe.getGuideEntriesForStar(
-        Universe.getIDForStar(tgt)
-      );
-      if (guideEntries.length === 0) {
-        guideEntries = Universe.getGuideEntriesForPlanetByName(tgt);
-      }
+(async () => {
+  try {
+    let commands = [lookupCommand.data.toJSON(), guideCommand.data.toJSON()];
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    const data = await rest.put(
+      (Routes as any).applicationCommands(config.clientId),
+      { body: commands },
+    );
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
-      let entriesText = "No entries found in GUIDE";
-      let entriesFooter = null;
-      if (guideEntries.length > 0) {
-        const offsetMax = offset + 10;
-        const displayGuideEntries = guideEntries.filter((x, index) => {
-          return index >= offset && index < offsetMax;
-        });
-        entriesText = displayGuideEntries.map(x => `> ${x.text}`).join("\n");
-        const remainingEntries = guideEntries.length - offsetMax;
-        if (remainingEntries > 0) {
-          entriesFooter = `${remainingEntries} more entries available: !guide ${tgt.toUpperCase()} (+${offsetMax})`;
-        } else {
-          entriesFooter = "No more entries remaining";
-        }
-      }
-      if (entriesText.length > 0) {
-        const embed = makeEmbed(
-          `Guide entries for ${tgt.toUpperCase()}:`,
-          entriesText
-        );
-        if (entriesFooter) {
-          embed.setFooter(entriesFooter);
-        }
-        msg.channel.send(embed);
-      } else {
-        msg.channel.send("No (more) entries found!");
-      }
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
     } else {
-      msg.channel.send("No object found!");
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
   }
 });
